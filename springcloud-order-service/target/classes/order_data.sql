acb507495d4b3e0692a683a0bdcf9a7f66dd1bf9
haifeng <haifeng.zuo@frontpay.cn>
2019-05-24 10:41:07 +0800

    删除远端忽略文件

--- springcloud-order-service/target/classes/order_data.sql
+++
@@ -1,3 +0,0 @@
-insert into order_Info (id, order_No, order_Remark) values (1, '20171111111111', '张三的订单', );
-insert into order_Info (id, order_No, order_Remark) values (2, '20172222222222', '李四的订单', );
-insert into order_Info (id, order_No, order_Remark) values (3, '20173333333333', '王五的订单', );
