475f59fb4cb30cbfaee06de72b04d87aa953f403
haifeng <haifeng.zuo@frontpay.cn>
2019-05-24 10:33:57 +0800

    本地代码调试

--- springcloud-eureka-server-ha/src/main/java/com/study/springcloud/springcloudeurekaserver/SpringcloudEurekaServerApplication.java
+++ springcloud-eureka-server-ha/src/main/java/com/study/springcloud/springcloudeurekaserver/SpringcloudEurekaServerApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringcloudEurekaServerApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
 	}
 }
+// 微服务项目的高可用做个简单的介绍，我们一般的实现方式为通过启动两个注册中心，相互注册，实现服务的相互同步来达到高可用的目的
+//配置HOST
+// 127.0.0.1 peer1
+// 127.0.0.1 peer2
--- springcloud-user-service/src/main/java/com/study/springcloud/springcloudordinaryuserservice/SpringcloudOrdinaryUserServiceApplication.java
+++ springcloud-user-service/src/main/java/com/study/springcloud/springcloudordinaryuserservice/SpringcloudOrdinaryUserServiceApplication.java
@@ -9,12 +9,12 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringcloudOrdinaryUserServiceApplication {
-	
-	 @Bean
-	 RestTemplate restTemplate() {
-		 return new RestTemplate();
-	 }
-	
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudOrdinaryUserServiceApplication.class, args);
 	}
